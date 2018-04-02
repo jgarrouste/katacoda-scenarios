02d7cf2a43a699ec95035f052b7cefdc111415fd
Jérémy GARROUSTE <jeremy.garroute@inpiwee.fr>
2018-04-02 14:13:22 +0200

    Add kubetail and disable stork auto-install

--- px-k8s-postgres-all-in-one/background.sh
+++ px-k8s-postgres-all-in-one/background.sh
@@ -1,3 +1,4 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
-kubectl create -f stork.yaml
-kubectl create -f stork-scheduler.yaml
+wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail && chmod +x kubetail
+#kubectl create -f stork.yaml
+#kubectl create -f stork-scheduler.yaml
