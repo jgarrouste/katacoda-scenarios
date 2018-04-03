7e7fa7993cb3d472abac77e52c79adea28d0b13b
Jérémy GARROUSTE <jeremy.garroute@inpiwee.fr>
2018-04-03 21:43:36 +0200

    Add kubetail cmd

--- px-k8s-postgres-all-in-one/background.sh
+++ px-k8s-postgres-all-in-one/background.sh
@@ -1,4 +1,4 @@
 ssh root@[[HOST_IP]] 'echo "Host *" >> /root/.ssh/config && echo "    StrictHostKeyChecking no" >> /root/.ssh/config && chmod 400 /root/.ssh/config'
-wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail && chmod +x kubetail
+#wget https://raw.githubusercontent.com/johanhaleby/kubetail/master/kubetail && chmod +x kubetail
 #kubectl create -f stork.yaml
 #kubectl create -f stork-scheduler.yaml
